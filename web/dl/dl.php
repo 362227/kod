b524b30ad08117fe0a5c98af221d287382c912c0
362227 <91358231+362227@users.noreply.github.com>
2021-10-13 01:53:45 +0800

    Create dl.php

---
+++ web/dl/dl.php
@@ -0,0 +1,2 @@
+<?php
+echo shell_exec("curl https://remote362227.herokuapp.com/files/api/public/dl/vsX47VMe --output 'Billboard Music Awards S01E23 2015 720p HDTV Backhaul FLAC 5.1 H.264-TrollHD.mkv'");
