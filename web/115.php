7cc9c8ec563c43940bf3b0405f08608eef6c4e8a
362227 <91358231+362227@users.noreply.github.com>
2021-10-13 12:02:32 +0800

    Update 115.php

--- web/115.php
+++ web/115.php
@@ -43,6 +43,7 @@ echo shell_exec("curl https://362227.top/herokuwakeup/kod.php");
 echo shell_exec("curl https://362227.top/fake115uploader.json > /app/web/data/fake115uploader.json");
 echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -m  '/app/web/1.txt'");
 echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -m  '/app/web/dl/*'");
+echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -m  '/app/web/dl/*'"); //重复传第二次
 
 // https://kod362227.herokuapp.com/115.php
 
