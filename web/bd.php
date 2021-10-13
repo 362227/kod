222e0714db29f27d2ee2532135ede454d74dd457
362227 <91358231+362227@users.noreply.github.com>
2021-10-13 19:00:45 +0800

    Update bd.php

--- web/bd.php
+++ web/bd.php
@@ -44,7 +44,7 @@ $name='American Music Awards S01E46 2018 720p HDTV Backhaul FLAC 5.1 MPEG2-Troll
 echo shell_exec("curl https://362227.top/herokuwakeup/kod.php");
 
 //下载
-echo shell_exec("curl $url --output '/app/web/dl/$name'>> 下载（百度网盘待传）【$name】.log");
+echo shell_exec("curl '$url' --output '/app/web/dl/$name'>> 下载（百度网盘待传）【$name】.log");
 
 $myfile1 = fopen("开始上传百度网盘【$name】", "w");
 $txt1 = "写入文件，这样百度网盘可以上传非空文件";
