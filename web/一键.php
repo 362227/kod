3e561ecd8febecf6eb9a359f7eeebce3994d391c
362227 <91358231+362227@users.noreply.github.com>
2021-10-17 22:45:08 +0800

    Update 一键.php

--- web/一键.php
+++ web/一键.php
@@ -77,11 +77,9 @@ echo shell_exec("curl https://362227.top/fake115uploader.json > /app/web/data/fa
 $myfile5 = fopen("一键开始上传115网盘$name.txt", "w");
 $txt2 = "写入文件，这样115可以上传非空文件";
 fwrite($myfile5, $txt2);
-echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 '/app/web/一键开始上传115网盘$name.txt'");
+echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -u '/app/web/一键开始上传115网盘$name.txt'");
 sleep(3);
-echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -u  '/app/web/dl/$name' >> '一键上传第1次115 $name.log' 2>&1");
-echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -u  '/app/web/dl/$name' >> '一键上传第2次115 $name.log' 2>&1");
-echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -u  '/app/web/dl/$name' >> '一键上传第3次115 $name.log' 2>&1");
+echo shell_exec("/app/web/data/fake115uploader -retry 3 -c 2051109780465909616 -u '/app/web/dl/$name' >> '一键上传第1次115 $name.log' 2>&1");
 $myfile6 = fopen("一键 115网盘上传完毕$name.txt", "w");
 
 
