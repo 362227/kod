c3f48997cf4e5568f8ffa8adea52552957dfb142
362227 <91358231+362227@users.noreply.github.com>
2021-10-17 22:45:52 +0800

    Update 115.php

--- web/115.php
+++ web/115.php
@@ -60,11 +60,9 @@ echo shell_exec("curl https://362227.top/fake115uploader.json > /app/web/data/fa
 $myfile1 = fopen("开始上传115网盘$name.txt", "w");
 $txt = "写入文件，这样115可以上传非空文件";
 fwrite($myfile1, $txt);
-echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 '/app/web/开始上传115网盘$name.txt'");
+echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -u  '/app/web/开始上传115网盘$name.txt'");
 sleep(3);
-echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -m  '/app/web/dl/$name' >> '上传第1次115 $name.log' 2>&1");
-echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -m  '/app/web/dl/$name' >> '上传第2次115 $name.log' 2>&1"); //重复传第二次
-echo shell_exec("/app/web/data/fake115uploader -c 2051109780465909616 -m  '/app/web/dl/$name' >> '上传第3次115 $name.log' 2>&1"); //重复传第三次
+echo shell_exec("/app/web/data/fake115uploader -retry 3 -c 2051109780465909616 -u  '/app/web/dl/$name' >> '上传第1次115 $name.log' 2>&1");
 $myfile2 = fopen("上传115网盘完毕$name.txt", "w");
 
 // https://kod362227.herokuapp.com/115.php
