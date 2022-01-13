2e78c9c9d19592adab90ed97ebba7a394a853da0
362227 <91358231+362227@users.noreply.github.com>
2022-01-13 21:36:52 +0800

    Create yt.php

---
+++ web/yt.php
@@ -0,0 +1,16 @@
+<?php
+$id=$_GET["id"];
+
+
+
+$cmd='/app/web/data/yt-dlp -f 18 https://www.youtube.com/watch?v='.$id.' -o '.$id.'.mp4';
+
+shell_exec($cmd.'>> yt.log');
+
+//shell_exec("ffmpeg -i $id.flv -vcodec copy -acodec copy -absf aac_adtstoasc $id.mp4");
+
+echo $cmd;
+
+$url = 'http://kod10362227.herokuapp.com/'.$id.'.mp4';
+echo $url;
+header('Location: ' . $url);
