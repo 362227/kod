8a2521ff5ba282a18d2938439d9abb82eed305df
362227 <91358231+362227@users.noreply.github.com>
2022-03-19 20:33:41 +0800

    Update vimeodl.php

--- web/vimeodl.php
+++ web/vimeodl.php
@@ -104,8 +104,8 @@ header("HTTP/1.1 200 OK"); //可以发送200状态码，以这些请求是成功
 //register_shutdown_function("runRack");
 //return  ;
 ob_start();//开始当前代码缓冲
-$arr = ["文件生成中"];
-echo json_encode($arr);
+$arr = '<p>勿刷新该页面！<a href="/vimeodl.php" class="item project">点击返回</a></p>';
+echo $arr;
 //下面输出http的一些头信息
 $size = ob_get_length();
 header("Content-Length: $size");
