936571105439b8795efe51722d714495617c4715
362227 <91358231+362227@users.noreply.github.com>
2022-03-19 14:30:51 +0800

    Update vimeodl.php

--- web/vimeodl.php
+++ web/vimeodl.php
@@ -57,7 +57,7 @@ if ($lenth>8) {
 shell_exec("rm -rf /app/web/vimeodl/*.part"); //删除临时文件
 shell_exec("pkill BaiduPCS-Go");
 shell_exec("pkill yt-dlp");
-
+sleep(3);
 	
 echo shell_exec("cd /app/web/vimeodl/ && /app/web/data/yt-dlp $url > /app/web/下载日志.txt");
 //$output = file_get_contents ("1.log");
