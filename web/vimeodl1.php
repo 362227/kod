2c5ffa72276cb9da7e4b085afe6a478ad85235b8
362227 <91358231+362227@users.noreply.github.com>
2022-03-21 14:32:12 +0800

    Update vimeodl1.php

--- web/vimeodl1.php
+++ web/vimeodl1.php
@@ -57,20 +57,19 @@ URL: <input type="text" name="url">
 <h1>Your request is processing. Terminal output below:</h1>
 <div class="box">
 <?php
-if(file_exists("vimeodl上传百度网盘.txt")){ $F=date(filemtime("vimeodl1上传百度网盘.txt"));}
-if(file_exists("vimeodl下载日志.txt")){ $G=date(filemtime("vimeodl1下载日志.txt.txt"));}
+if(file_exists("vimeodl1上传百度网盘.txt")){ 
+    $F=date(filemtime("vimeodl1上传百度网盘.txt"));
+    $Y = $A- $F;  //上传百度网盘最后修改
+    $fp = file("vimeodl1上传百度网盘.txt");
+    $lastline = $fp[count($fp)-1]; //最后一行
+    if($Y < 5 || $lastline === '[1] 检测秒传中, 请稍候...'|| $lastline === '[2] 检测秒传中, 请稍候...') {echo '<caption><h3><font color="#FF0000">正在执行任务，请勿添加新链接，否则本任务会被强行取消</font></h3></caption>';}
+}
 
-$A=strtotime("now");
-
-$X = $A- $G;
-$Y = $A- $F;  //上传百度网盘最后修改
-
-$fp = file("vimeodl1上传百度网盘.txt");
-$lastline = $fp[count($fp)-1]; //最后一行
-
-
-if($X < 5 || $Y < 5 || $lastline === '[1] 检测秒传中, 请稍候...'|| $lastline === '[2] 检测秒传中, 请稍候...') {echo '<caption><h3><font color="#FF0000">正在执行任务，请勿添加新链接，否则本任务会被强行取消</font></h3></caption>';}
-	
+if(file_exists("vimeodl1下载日志.txt")){ 
+    $G=date(filemtime("vimeodl1下载日志.txt"));
+    $X = $A- $G;
+    if($X < 6) {echo '<caption><h3><font color="#FF0000">正在执行任务，请勿添加新链接，否则本任务会被强行取消</font></h3></caption>';}
+}	
 	
 unlink("/app/web/vimeodl1/0000");
 $actual_link = 'https://'.$_SERVER['HTTP_HOST'];
