842139a11fbcb47ce014361d86d71ac028545f33
362227 <91358231+362227@users.noreply.github.com>
2022-03-21 10:04:01 +0800

    Update vimeodl.php

--- web/vimeodl.php
+++ web/vimeodl.php
@@ -70,6 +70,8 @@ $date = date('Y-m-d-H-i-s');
 shell_exec("mkdir $date");
 	
 shell_exec("rm -rf /app/web/vimeodl/*.part"); //删除临时文件
+shell_exec("rm -rf /app/web/vimeodl/*.ytdl"); //删除临时文件
+shell_exec("rm -rf /app/web/vimeodl/*.part-Frag*"); //删除临时文件
 shell_exec("rm -rf /app/web/vimeodl上传百度网盘.txt"); //删除临时文件
 shell_exec("rm -rf /app/web/vimeodl下载日志.txt"); //删除临时文件
 //shell_exec("pkill BaiduPCS-Go");
