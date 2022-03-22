f444ac091e1bbd2771372c5f6bb80de9cc1c5b1f
362227 <91358231+362227@users.noreply.github.com>
2022-03-22 10:27:06 +0800

    Update vimeodl1.php

--- web/vimeodl1.php
+++ web/vimeodl1.php
@@ -49,7 +49,7 @@ body{
 <h1>youtube-dl</h1>
 <p>Paste the URL below, and press GO.</p>
 <form action="vimeodl1.php" method="get">
-URL: <input type="text" name="url" style="font-size:25px">
+URL: <input type="text" name="url" style="font-size:26px">
 <input type="submit" value="下载" style="font-size:23px">
 </form>
 
@@ -470,7 +470,7 @@ $actual_link = 'https://'.$_SERVER['HTTP_HOST'];
 $page = file_get_contents($actual_link.'/encodeexplorer.index.php?m&sort_by=mod&sort_as=desc&dir=115/');
 
 $page = str_replace("src=\"?img","src=\"encodeexplorer.index.php?img",$page);
-$page = str_replace("<a href=\"?s&amp;dir=\">","<a href=\"encodeexplorer.index.php?sort_by=mod&sort_as=desc&dir=115/\">",$page);
+$page = str_replace("<a href=\"?s&amp;dir=115/\">","<a href=\"encodeexplorer.index.php?sort_by=mod&sort_as=desc&dir=115/\">",$page);
 echo '<hr /><br>'.$page;
 	
 	
