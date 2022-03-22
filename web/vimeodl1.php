0850c71742b643a2687badd46894679665f7057d
362227 <91358231+362227@users.noreply.github.com>
2022-03-22 15:07:09 +0800

    Update vimeodl1.php

--- web/vimeodl1.php
+++ web/vimeodl1.php
@@ -292,9 +292,7 @@ echo '<hr /><br>'.$page;
 ?>
 </p>
 
-<p><a href="vimeodl1下载日志.txt">vimeodl1下载日志</a></p>
-<p><a href="vimeodl1上传百度网盘日志.php">上传百度网盘日志</a></p>
-<p><a href="上传115.txt">上传115网盘日志</a></p>
+<p><a href="vimeodl1下载日志.txt"><font size="2">下载日志</font></a>  ·  <a href="vimeodl1上传百度网盘日志.php"><font size="2">上传百度网盘日志</font></a>  ·  <a href="上传115.txt"><font size="2">上传115网盘日志</font></a></p>
 
 </body>
 </html>
