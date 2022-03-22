a1d3ad037c7341f8116508dc1e7814e181465e6b
362227 <91358231+362227@users.noreply.github.com>
2022-03-22 11:12:00 +0800

    Update vimeodl.php

--- web/vimeodl.php
+++ web/vimeodl.php
@@ -211,7 +211,7 @@ if($C < 4) {echo '<caption><h1><font color="#FF0000">正在上传115，请只打
 
 
 $url1 = file_get_contents("vimeodlurl.txt");
-$url2 = preg_replace('/(^http|^youtube|^vimeo|^www)([\s\S]{8,60}).*/','$1$2', $url1); //取url前8-60个作为超链名字
+$url2 = preg_replace('/(^http|^youtube|^vimeo|^www)([\s\S]{8,50}).*/','$1$2', $url1); //取url前8-50个作为超链名字
 echo '<p><a href="'.$url1.'">'.$url2.'</a></p>';
 
 echo '<caption><h3>下载进度</h3></caption>';
