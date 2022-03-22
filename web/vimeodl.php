a76029843bc201d15d5ca884d286cad76916b455
362227 <91358231+362227@users.noreply.github.com>
2022-03-22 10:15:23 +0800

    Update vimeodl.php

--- web/vimeodl.php
+++ web/vimeodl.php
@@ -230,7 +230,7 @@ $actual_link = 'https://'.$_SERVER['HTTP_HOST'];
 $page = file_get_contents($actual_link.'/encodeexplorer.index.php?m&sort_by=mod&sort_as=desc&dir=115/');
 
 $page = str_replace("src=\"?img","src=\"encodeexplorer.index.php?img",$page);
-//$page = str_replace("<span>Encode Explorer</span>","<span>已离线的文件，每个文件大约1-2小时后删</span>",$page);
+$page = str_replace("<a href=\"?s&amp;dir=\">","<a href=\"encodeexplorer.index.php?sort_by=mod&sort_as=desc&dir=115/\">",$page);
 echo $page;
 	
 	
