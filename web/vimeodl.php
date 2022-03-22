9a3ecd282610f13ec911b6ffaea514a7cafdf10f
362227 <91358231+362227@users.noreply.github.com>
2022-03-22 10:23:32 +0800

    Update vimeodl.php

--- web/vimeodl.php
+++ web/vimeodl.php
@@ -230,7 +230,7 @@ $actual_link = 'https://'.$_SERVER['HTTP_HOST'];
 $page = file_get_contents($actual_link.'/encodeexplorer.index.php?m&sort_by=mod&sort_as=desc&dir=115/');
 
 $page = str_replace("src=\"?img","src=\"encodeexplorer.index.php?img",$page);
-$page = str_replace("<a href=\"?s&amp;dir=\">","<a href=\"encodeexplorer.index.php?sort_by=mod&sort_as=desc&dir=115/\">",$page);
+$page = str_replace("<a href=\"?s&amp;dir=115/\">","<a href=\"encodeexplorer.index.php?sort_by=mod&sort_as=desc&dir=115/\">",$page);
 echo '<hr /><br>'.$page;
 	
 	
