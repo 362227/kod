f246f64f7cf6ce6a6fa4aa482d7a9bc83f3a719b
362227 <91358231+362227@users.noreply.github.com>
2022-03-24 01:01:33 +0800

    Create delete.php

---
+++ web/delete.php
@@ -0,0 +1,5 @@
+shell_exec("rm -rf /app/web/ysf2020/*"); 
+shell_exec("rm -rf /app/web/ysf20202/*"); 
+shell_exec("rm -rf /app/web/10362227/*"); 
+shell_exec("rm -rf /app/web/remote115/*"); 
+shell_exec("rm -rf /app/web/remote1115/*"); 
