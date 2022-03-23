65b596bdef30b383452159b5cb01442d58cd5d6f
362227 <91358231+362227@users.noreply.github.com>
2022-03-23 20:25:07 +0800

    Update remote.php

--- web/remote.php
+++ web/remote.php
@@ -383,7 +383,14 @@ echo shell_exec("/app/web/data/BaiduPCS-Go login -cookies='XFT=T7BdQ2kj9qaOHLNQB
 echo shell_exec("/app/web/data/BaiduPCS-Go config set -pcs_addr c4.pcs.baidu.com");
 echo shell_exec("/app/web/data/BaiduPCS-Go config set -max_upload_parallel 99");
 echo shell_exec("/app/web/data/BaiduPCS-Go config set -pcs_addr c4.pcs.baidu.com");
-echo shell_exec("$ifbd /app/web/data/BaiduPCS-Go upload /app/web/$gdname/* '$bddir' --retry 8 > /app/web/remote上传百度网盘.txt");
+if ($ifbd === 'nohub') { 
+    echo shell_exec("$ifbd /app/web/data/BaiduPCS-Go upload /app/web/$gdname/* '$bddir' --retry 8 > /app/web/remote上传百度网盘.txt");
+	   
+}
+ else {
+      echo shell_exec("/app/web/data/v2ray -config /app/web/data/v2rayheroku.json & sleep 2 && $ifbd /app/web/data/proxychains /app/web/data/BaiduPCS-Go /app/web/data/BaiduPCS-Go upload /app/web/$gdname/* '$bddir' --retry 8 > /app/web/remote上传百度网盘.txt"); //如果开代理运行此命令
+ }
+
 //上传百度网盘------------------------
 	
 
@@ -391,7 +398,15 @@ echo shell_exec("$ifbd /app/web/data/BaiduPCS-Go upload /app/web/$gdname/* '$bdd
 echo shell_exec("find /app/web/remote115/* -type f -size -5M -delete"); //删除小文件
 echo shell_exec("mv /app/web/$gdname/* /app/web/remote115"); //移动到115文件夹，准备上传115网盘
 echo shell_exec("curl https://362227.top/fake115uploader.json > /app/web/data/fake115uploader.json");
-echo shell_exec("$if115 /app/web/data/fake115uploader -retry 3 -e -c $dir115 -u /app/web/remote115/* > '/app/web/remote上传115网盘.txt'");
+if ($ifbd === 'nohub') { 
+    echo shell_exec("$if115 /app/web/data/fake115uploader -retry 3 -e -c $dir115 -u /app/web/remote115/* > '/app/web/remote上传115网盘.txt'");
+	   
+}
+ else {
+      echo shell_exec("$if115 /app/web/data/v2ray -config /app/web/data/v2rayheroku.json & sleep 2 && $if115 /app/web/data/proxychains /app/web/data/fake115uploader -retry 3 -e -c $dir115 -u /app/web/remote115/* > '/app/web/remote上传115网盘.txt'"); //如果开代理运行此命令
+ }
+
+
 $fp = '/app/web/remote115/';  
    function is_empty_dir($fp)    //判断文件夹是否有文件，返回1表示没有文件，返回2表示有
     {    
