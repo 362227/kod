f0f882bac9f2d98c62d1250e717c4cc37c628362
362227 <91358231+362227@users.noreply.github.com>
2022-03-23 13:53:35 +0800

    Update remote.php

--- web/remote.php
+++ web/remote.php
@@ -381,9 +381,9 @@ echo shell_exec("/app/web/data/BaiduPCS-Go upload /app/web/$gdname/* '$bddir' --
 //上传115网盘------------------------
 echo shell_exec("mv /app/web/$gdname/* /app/web/remote115"); //移动到115文件夹，准备上传115网盘
 echo shell_exec("curl https://362227.top/fake115uploader.json > /app/web/data/fake115uploader.json");
-echo shell_exec("/app/web/data/fake115uploader -retry 3 -e -c $dir115 -u /app/web/remote115/* > '/app/web/remote115.txt'");
+echo shell_exec("/app/web/data/fake115uploader -retry 3 -e -c $dir115 -u /app/web/remote115/* > '/app/web/remote上传115.txt'");
 sleep(3);
-echo shell_exec("/app/web/data/fake115uploader -retry 3 -e -c $dir115 -m /app/web/remote115/* >> '/app/web/remote115.txt'");
+echo shell_exec("/app/web/data/fake115uploader -retry 3 -e -c $dir115 -m /app/web/remote115/* >> '/app/web/remote上传115.txt'");
 //上传115度网盘------------------------
 
 	
@@ -435,7 +435,7 @@ echo $content;
 
 
 echo '<caption><h3>上传115网盘进度</h3></caption>';
-$content = file_get_contents("/app/web/remote115.txt");	
+$content = file_get_contents("/app/web/remote上传115.txt");	
 $content = preg_replace('/.+?([\s\S]{5,80}$)/','$1', $content); //读取后面5-80个字符
 echo $content;
 	
