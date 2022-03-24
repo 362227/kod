47d5c323fbeb0903a8233d6745600f73a2e8eaae
362227 <91358231+362227@users.noreply.github.com>
2022-03-25 00:15:22 +0800

    Create vimeodl上传115网盘.php

---
+++ web/vimeodl上传115网盘.php
@@ -0,0 +1,13 @@
+<?php
+$file_path = "/app/web/vimeodl上传115网盘.txt" ;
+if ( file_exists ( $file_path )){
+$file_arr = file( $file_path );
+for ( $i =0; $i < count ( $file_arr ); $i ++){ //逐行读取文件内容
+echo $file_arr [ $i ]. "<br />" ;
+}
+/*
+foreach($file_arr as $value){
+echo $value."<br />";
+}*/
+}
+?>
