e12b5ed5d3f7bb2c24b79c84aa5244402892e345
362227 <91358231+362227@users.noreply.github.com>
2022-03-24 12:17:33 +0800

    Update killall.php

--- web/killall.php
+++ web/killall.php
@@ -1,16 +1,10 @@
 <?php
 shell_exec("pkill aria2c"); 
-sleep 1;
 shell_exec("pkill aria2c"); 
-sleep 1;
 shell_exec("pkill youtube-dl"); 
-sleep 1;
 shell_exec("pkill yt-dlp"); 
-sleep 1;
 shell_exec("pkill rclone"); 
-sleep 1;
 shell_exec("pkill BaiduPCS-Go"); 
-sleep 1;
 shell_exec("pkill fake115uploader"); 
 
 
