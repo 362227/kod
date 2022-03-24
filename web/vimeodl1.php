225e06693f8fa1bece9fd427f9cffbeea0857827
362227 <91358231+362227@users.noreply.github.com>
2022-03-24 19:08:07 +0800

    Update vimeodl1.php

--- web/vimeodl1.php
+++ web/vimeodl1.php
@@ -320,9 +320,9 @@ sleep(3);
 	
 	
 if ($renamelenth>1) {
-    echo shell_exec("cd /app/web/vimeodl1/ && /app/web/data/youtube-dl -i vimeodl1url.txt -o '$rename' --no-mtime > /app/web/vimeodl1下载日志.txt");}
+    echo shell_exec("cd /app/web/vimeodl1/ && /app/web/data/youtube-dl -a /app/web/vimeodl1url.txt -o '$rename' --no-mtime > /app/web/vimeodl1下载日志.txt");}
     else  {
-        echo shell_exec("cd /app/web/vimeodl1/ && /app/web/data/youtube-dl -i vimeodl1url.txt --no-mtime > /app/web/vimeodl1下载日志.txt");}
+        echo shell_exec("cd /app/web/vimeodl1/ && /app/web/data/youtube-dl -a /app/web/vimeodl1url.txt --no-mtime > /app/web/vimeodl1下载日志.txt");}
 //$output = file_get_contents ("1.log");
 //echo $output;
 
