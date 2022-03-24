8d48ff8d8c5a19b905a499e5e463fe5d86002f77
362227 <91358231+362227@users.noreply.github.com>
2022-03-25 00:09:27 +0800

    Update vimeodl强制所有上传115.php

--- web/vimeodl强制所有上传115.php
+++ web/vimeodl强制所有上传115.php
@@ -51,7 +51,7 @@ shell_exec("find /app/web/115/* -type f -size -5M -delete"); //删除小文件
 
 
 
-/
+
 
 echo shell_exec("pkill fake115uploader");
 
@@ -95,7 +95,7 @@ if($num > 0) {
     echo shell_exec("/app/web/data/fake115uploader -retry 5 -e -c 2214093215491948252 -m /app/web/115/* >> '上传115.txt' 2>&1"); //上传“手机备份”文件夹
 }  
 
-}
+
 
 
 
