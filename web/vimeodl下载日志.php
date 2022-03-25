e0e3836f935cdbfb7e1a283a00de68bafb64d00a
362227 <91358231+362227@users.noreply.github.com>
2022-03-25 18:59:02 +0800

    Update vimeodl下载日志.php

--- web/vimeodl下载日志.php
+++ web/vimeodl下载日志.php
@@ -3,7 +3,7 @@ $file_path = "/app/web/vimeodl下载日志.txt" ;
 if ( file_exists ( $file_path )){
 $file_arr = file( $file_path );
 for ( $i =0; $i < count ( $file_arr ); $i ++){ //逐行读取文件内容
-echo $file_arr [ $i ]. "<br />" ;
+echo $file_arr [ $i ]. "<br>" ;
 }
 /*
 foreach($file_arr as $value){
