0b54c128fa3852066a478b7658632a6fd7fd7511
362227 <91358231+362227@users.noreply.github.com>
2022-03-26 02:11:29 +0800

    Update vimeodl1.php

--- web/vimeodl1.php
+++ web/vimeodl1.php
@@ -466,7 +466,7 @@ echo $content;
 	
 $url1 = file_get_contents("vimeodl1url.txt");
 $url2 = preg_replace('/(^http|^youtube|^vimeo|^www)([\s\S]{8,50}).*/','$1$2', $url1); //取url前8-50个作为超链名字
-echo '<p>链接：<a href="'.$url1.'">'.$url1.'</a></p>';
+echo '<p>链接：<a href="vimeodl1url.txt" target="_blank">'.$url1.'</a></p>';
 
 echo '<caption><h3>下载进度</h3></caption>';
 //$content = file_get_contents("vimeodl1下载日志.txt");
