8631ce68ee36e610c9c2eca7b07aa793150df1a5
362227 <91358231+362227@users.noreply.github.com>
2022-03-25 23:09:32 +0800

    Update vimeodl1.php

--- web/vimeodl1.php
+++ web/vimeodl1.php
@@ -290,16 +290,11 @@ $renamelenth=strlen($rename); //获取rename长度
 if ($lenth>8) {
 $url = str_replace(array(' ','\n'), array('#','#'), $url);
 
-$arr = array(
-    'https://362227.top/rss/vimeo10362227.php?id=',
-    'http://193.123.232.200/kodexplorer/rss/vimeo.php?id='
-   
-);
-$key = array_rand($arr, 1);
 
- if (preg_match('/(http\:\/\/vimeo\.com)|(https\:\/\/vimeo\.com)/', $url, $url1)) {
-$url = $arr[$key].$url;
-}
+$url = str_replace(array('http://vimeo.com','https://vimeo.com'), array('https://362227.top/rss/vimeo10362227.php?id=http://vimeo.com','https://362227.top/rss/vimeo10362227.php?id=http://vimeo.com'), $url);
+
+
+
 else {$url=$url;}
 file_put_contents('vimeodl1url.txt', $url);
 $date = date('Y-m-d-H-i-s');
