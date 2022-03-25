b010c6c3b02feb8ded7489e0826879df2e409f6f
362227 <91358231+362227@users.noreply.github.com>
2022-03-25 22:42:54 +0800

    Update vimeodl1.php

--- web/vimeodl1.php
+++ web/vimeodl1.php
@@ -289,6 +289,18 @@ $lenth=strlen($url); //获取url长度
 $renamelenth=strlen($rename); //获取rename长度
 if ($lenth>8) {
 $url = str_replace(array(' ','\n'), array('#','#'), $url);
+$url = $_GET['url'];
+$arr = array(
+    'https://362227.top/rss/vimeo10362227.php?id=',
+    'http://193.123.232.200/kodexplorer/rss/vimeo.php?id='
+   
+);
+$key = array_rand($arr, 1);
+
+ if (preg_match('/(http\:\/\/vimeo\.com)|(https\:\/\/vimeo\.com)/', $url, $url1)) {
+$url = $arr[$key].$url;
+}
+else {$url=$url;}
 file_put_contents('vimeodl1url.txt', $url);
 $date = date('Y-m-d-H-i-s');
 shell_exec("mkdir $date");
