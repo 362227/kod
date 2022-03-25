876ed742eee49aa1c6fb35a65dc87c59a32dd0c3
362227 <91358231+362227@users.noreply.github.com>
2022-03-26 00:28:46 +0800

    Update vimeodl1上传百度网盘日志.php

--- web/vimeodl1上传百度网盘日志.php
+++ web/vimeodl1上传百度网盘日志.php
@@ -1,5 +1,5 @@
 <?php
-$file_path = "vimeodl1url.txt" ;
+$file_path = "vimeodl1上传百度网盘日志.txt" ;
 if ( file_exists ( $file_path )){
 $file_arr = file( $file_path );
 for ( $i =0; $i < count ( $file_arr ); $i ++){ //逐行读取文件内容
@@ -7,10 +7,10 @@ $resault = $file_arr [ $i ]. "<br />" ;
 
 }
 
-if (preg_match_all('/上传结束/', file_get_contents('vimeodl1url.txt', $url), $links)){
+if (preg_match_all('/上传结束/', file_get_contents('vimeodl1上传百度网盘日志..txt', $url), $links)){
     
 
-echo '<font color="#F12A0B">'.preg_replace('/[\s\S]*(上传结束.*)/','$1', file_get_contents('vimeodl1url.txt')).'</font>';
+echo '<font color="#F12A0B">'.preg_replace('/[\s\S]*(上传结束.*)/','$1', file_get_contents('vimeodl1上传百度网盘日志.txt')).'</font>';
 
     }
 
