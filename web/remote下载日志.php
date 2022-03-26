964e68a52c4bfdfc1f262c4dba67f251b1b645aa
362227 <91358231+362227@users.noreply.github.com>
2022-03-26 09:29:36 +0800

    Create remote下载日志.php

---
+++ web/remote下载日志.php
@@ -0,0 +1,16 @@
+<?php
+$file_path = "/app/web/remote下载日志.txt" ;
+if ( file_exists ( $file_path )){
+$file_arr = file( $file_path );
+for ( $i =0; $i < count ( $file_arr ); $i ++){ //逐行读取文件内容
+//echo $file_arr [ $i ]. "<br>" ;
+$content = $file_arr [ $i ]. "<br>" ;
+$content = preg_replace('/[\s\S]*(\[download\].*)/','$1', $content);
+ echo $content;
+}
+/*
+foreach($file_arr as $value){
+echo $value."<br>";
+}*/
+}
+?>
