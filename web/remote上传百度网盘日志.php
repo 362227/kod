986cd280ba3c21255dad30f1fc76928343ffca52
362227 <91358231+362227@users.noreply.github.com>
2022-03-26 09:30:30 +0800

    Update remote上传百度网盘日志.php

--- web/remote上传百度网盘日志.php
+++ web/remote上传百度网盘日志.php
@@ -1,13 +1,20 @@
 <?php
-$file_path = "/app/web/remote上传百度网盘.txt" ;
+$file_path = "remote上传百度网盘日志.txt" ;
 if ( file_exists ( $file_path )){
 $file_arr = file( $file_path );
 for ( $i =0; $i < count ( $file_arr ); $i ++){ //逐行读取文件内容
-echo $file_arr [ $i ]. "<br />" ;
+$resault = $file_arr [ $i ]. "<br />" ;
+
 }
-/*
-foreach($file_arr as $value){
-echo $value."<br />";
-}*/
+
+if (preg_match_all('/上传结束/', file_get_contents('remote上传百度网盘日志.txt', $url), $links)){
+    
+
+echo '<font color="#F12A0B">'.preg_replace('/[\s\S]*(上传结束.*)/','$1', file_get_contents('remote上传百度网盘日志.txt')).'</font>';
+
+    }
+
+else {
+for ( $i =0; $i < count ( $file_arr ); $i ++){ //逐行读取文件内容
+echo $file_arr [ $i ]. "<br />" ;}
 }
-?>
