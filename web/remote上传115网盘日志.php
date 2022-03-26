0fe588d6aee2f57961e4d5bd9809b8dea8b782d8
362227 <91358231+362227@users.noreply.github.com>
2022-03-26 13:58:58 +0800

    Update remote上传115网盘日志.php

--- web/remote上传115网盘日志.php
+++ web/remote上传115网盘日志.php
@@ -1,4 +1,5 @@
 <?php
+echo '<a href="查看UTF8完整版日志.php?file=remote上传115网盘日志.txt" target="_blank"><h3>下载进度</h3></a>';
 $content = file_get_contents("/app/web/remote上传115网盘日志.txt");	
 $content1 = preg_replace('/[\s\S]*([\s\S]{280}$)/','$1', $content); //读取后面280个字符
 echo $content1;
