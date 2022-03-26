55d4fed122129cfd86c8dc2fa46eb31003fd3b97
362227 <91358231+362227@users.noreply.github.com>
2022-03-26 13:34:06 +0800

    Update remote下载日志.php

--- web/remote下载日志.php
+++ web/remote下载日志.php
@@ -1,16 +1,41 @@
 <?php
-$file_path = "/app/web/remote下载日志.txt" ;
+$file_path = "remote下载日志.txt" ;
+
+$A=strtotime("now");
+$B=date(filemtime($file_path));
+
+$C= $A-$B;
+
+if ( $C < 5) { 
 if ( file_exists ( $file_path )){
 $file_arr = file( $file_path );
 for ( $i =0; $i < count ( $file_arr ); $i ++){ //逐行读取文件内容
 //echo $file_arr [ $i ]. "<br>" ;
 $content = $file_arr [ $i ]. "<br>" ;
-$content = preg_replace('/[\s\S]*(\[download\].*)/','$1', $content);
+$content= preg_replace('/[\s\S]*(\[download\].*)/','$1', $content);
  echo $content;
 }
-/*
-foreach($file_arr as $value){
-echo $value."<br>";
-}*/
+
+
 }
+}
+
+else {
+
+if ( file_exists ( $file_path )){
+$file_arr = file( $file_path );
+for ( $i =0; $i < count ( $file_arr ); $i ++){ //逐行读取文件内容
+//echo $file_arr [ $i ]. "<br>" ;
+$content = preg_replace('/.*\: Downloading.*/','', $file_arr);
+$content = $file_arr [ $i ]. "" ;
+$content = preg_replace('/[\s\S]*(\[download\].*)/','$1', $content);
+$content = preg_replace('/.*\: Downloading.*/','', $content);
+$content = preg_replace('/(^\[.*)/','<br>$1$2', $content);
+
+ echo $content;
+                                             }
+                  }
+
+}
+
 ?>
