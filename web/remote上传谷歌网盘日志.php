73ca136f6b4c4a784cdbea090f305081dae5f734
362227 <91358231+362227@users.noreply.github.com>
2022-03-27 20:22:42 +0800

    Update remote上传谷歌网盘日志.php

--- web/remote上传谷歌网盘日志.php
+++ web/remote上传谷歌网盘日志.php
@@ -1,7 +1,7 @@
 <?php
 
 $file =file_get_contents('remote上传谷歌网盘日志.txt');
-if(file_exists("remote上传谷歌网盘日志.txt")) {echo '<a href="查看UTF8完整版日志.php?file='.$file_path.'" target="_blank"><h3>上传谷歌网盘进度</h3></a>';}
+if(file_exists("remote上传谷歌网盘日志.txt")) {echo '<a href="查看UTF8完整版日志.php?file=remote上传谷歌网盘日志.txt'.$file_path.'" target="_blank"><h3>上传谷歌网盘进度</h3></a>';}
 
 $file1 = preg_replace('/[\s\S]*(\* [\s\S]*Elapsed time.*)/','$1', $file); 
 $file2 = preg_replace('/\n|[\s\S]*Elapsed time.+?\/ 0 Bytes.*/','<br>', $file1); 
