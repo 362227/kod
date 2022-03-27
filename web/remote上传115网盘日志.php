c9b1a0bb5cf760021df67cb1da4d49f0e0942331
362227 <91358231+362227@users.noreply.github.com>
2022-03-27 11:19:40 +0800

    Update remote上传115网盘日志.php

--- web/remote上传115网盘日志.php
+++ web/remote上传115网盘日志.php
@@ -1,8 +1,25 @@
 <?php
-if(file_exists("remote上传115网盘日志.txt")) {echo '<a href="查看UTF8完整版日志.php?file=remote上传115网盘日志.txt" target="_blank"><h3>上传115网盘进度</h3></a>';
+$file_path = "remote上传115网盘日志.txt";
+if(file_exists($file_path)) {echo '<a href="查看UTF8完整版日志.php?file=remote上传115网盘日志.txt" target="_blank"><h3>上传115网盘进度</h3></a>';
 
-$content = file_get_contents("/app/web/remote上传115网盘日志.txt");	
+$A=strtotime("now");
+$B=date(filemtime($file_path));
+
+
+if ( $C < 5) { 
+    
+$content = file_get_contents($file_path);	
 $content1 = preg_replace('/[\s\S]*([\s\S]{280}$)/','$1', $content); //读取后面280个字符
 echo $content1;
-                                 }
+}               
+ 
+else {
+$content = file_get_contents($file_path);	
+$content1 = preg_replace('/[\s\S]*([\s\S]{280}$)/','$1', $content); //读取后面280个字符
+$content2 = preg_replace('/[\s\S]*上传成功的文件/','上传成功的文件', $content1); //读取后面280个字符
+echo $content2;
+ 
+ 
+}   
+}
 ?>
