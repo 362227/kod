a4a41cda9abefcc1c8348f87b2ec756bb290f86e
362227 <91358231+362227@users.noreply.github.com>
2022-03-27 23:12:39 +0800

    Update vimeodl1.php

--- web/vimeodl1.php
+++ web/vimeodl1.php
@@ -561,9 +561,7 @@ file_put_contents('vimeodl1url.txt', $url);
 	
 $string = file_get_contents('vimeodl1url.txt');
  
-$linkstxt = preg_replace('/(^.{22}).*(.{22}$)/','$1.....$2', $links); //取url前22个和后22个作为超链名字
-$linkslink = preg_replace('/(.*)/','$1', $links); //取url完整
-$links = '<a href="'.$linkslink.'">'.$linkstxt.'</a><br>';
+$links =  preg_replace('/(.{0,51})(.*)/', '<a href="$1$2">$1</a><br>', $string); 
 file_put_contents('vimeodl1htmlurl.txt', $links);
 	
 	
