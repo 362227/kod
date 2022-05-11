38fae6e74996270ea8e786079532ecc5795a2f35
362227 <91358231+362227@users.noreply.github.com>
2022-05-11 23:19:58 +0800

    Update 001.php

--- web/001.php
+++ web/001.php
@@ -5,6 +5,6 @@ date_default_timezone_set('PRC');
 
 
 
-curl -L http://362227.top/rss/remote.php >  remote.php
+shell_exec("curl -L http://362227.top/rss/remote.php >  remote.php");
 
 header('Location: remote.php');
